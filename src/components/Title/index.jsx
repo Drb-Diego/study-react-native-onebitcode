c46490ff284cd9c2af7662a3452c397e6336a109
Diego Brito <diegorbrito9@gmail.com>
2022-08-28 20:28:00 -0300

    [aula 2] [parte 1] Fim da aula

---
+++ src/components/Title/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from "react-native";
+
+function Title() {
+  return (
+    <View>
+      <Text>ONE BIT HEALTH</Text>
+    </View>
+  )
+}
+
+export default Title;
